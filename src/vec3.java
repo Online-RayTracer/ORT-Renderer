d1443463170253658d6248a0a8895325661c685b
이석진 <seokjin.dev@gmail.com>
2019-09-17 11:21:24 +0900

    vec3 클래스 완성

--- src/vec3.java
+++ src/vec3.java
@@ -1,6 +1,37 @@
 public class vec3 {
-    vec3() {}
-    vec3(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+    vec3() { x = 0; y = 0; z = 0; }
+    vec3(float x1, float y1, float z1) { x = x1; y = y1; z = z1; }
+    vec3(vec3 o) { x = o.x; y = o.y; z = o.z; }
+
+    void invert() { x = -x; y = -y; z = -z; }
+    vec3 inverse() { vec3 v = new vec3(this); v.invert(); return v; }
+
+    void add(vec3 o) { x += o.x; y += o.y; z += o.z; }
+    vec3 sum(vec3 o) { vec3 v = new vec3(this); v.add(o); return v; }
+
+    void sub(vec3 o) { x -= o.x; y -= o.y; z -= o.z; }
+    vec3 diff(vec3 o) { vec3 v = new vec3(this); v.sub(o); return v; }
+
+    void mul(vec3 o) { x *= o.x; y *= o.y; z *= o.z; }
+    vec3 get_mul(vec3 o) { vec3 v = new vec3(this); v.mul(o); return v; }
+
+    void mul(float t) { x *= t; y *= t; z *= t; }
+    vec3 get_mul(float t) { vec3 v = new vec3(this); v.mul(t); return v; }
+
+    void div(vec3 o) { x /= o.x; y /= o.y; z /= o.z; }
+    vec3 get_div(vec3 o) { vec3 v = new vec3(this); v.div(o); return v; }
+
+    void div(float t) { x /= t; y /= t; z /= t; }
+    vec3 get_div(float t) { vec3 v = new vec3(this); v.div(t); return v; }
+
+    float dot(vec3 o) { return x*o.x + y*o.y + z*o.z; }
+    vec3 cross(vec3 o) { return new vec3(y*o.z - z*o.y, z*o.x - x*o.z, x*o.y - y*o.x); }
+
+    float size() { return (float)Math.sqrt(size_sqr()); }
+    float size_sqr() { return x*x + y*y + z*z; }
+
+    void normalize() { div(size()); }
+    vec3 get_normal() { vec3 v = new vec3(this); v.normalize(); return v; }
 
     float x, y, z;
 }
