7e73de6335f2a893669d06c2f2c8a430638db848
이석진 <seokjin.dev@gmail.com>
2019-09-18 11:23:07 +0900

    add ray class

---
+++ src/ray.java
@@ -0,0 +1,8 @@
+public class ray {
+    ray() {}
+    ray(vec3 origin, vec3 dir) { this.origin = origin; this.dir = dir; }
+
+    vec3 point_at(float t) { return origin.sum(dir.get_mul(t)); }
+
+    vec3 origin, dir;
+}
